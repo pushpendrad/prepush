9f2a8b3eabc419302f5d87e7fb12fee28b007f10
Pushpendra Dewangan <pushpendrad@cybage.com>
2018-12-03 12:43:23 +0530

    sp

--- src/main/java/com/sample/mvc/controller/EmployeeController.java
+++ src/main/java/com/sample/mvc/controller/EmployeeController.java
@@ -3,7 +3,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import springframework.ui.Model;
+import pringframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
